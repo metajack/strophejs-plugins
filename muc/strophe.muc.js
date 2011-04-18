a8858309495a5c980cdb36973da11cd02e6d2792
Matt Penning <matt@penning.com>
2011-04-17 19:27:13 -0700

    Fix loop logic in saveConfiguration

--- muc/strophe.muc.js
+++ muc/strophe.muc.js
@@ -191,7 +191,7 @@ Strophe.addConnectionPlugin('muc', {
                           type: "set"})
             .c("query", {xmlns: Strophe.NS.MUC_OWNER})
             .c("x", {xmlns: "jabber:x:data", type: "submit"});
-        for (var i = 0; i >= configarray.length; i++) {
+        for (var i = 0; i < configarray.length; i++) {
             config.cnode(configarray[i]);
         }
         var stanza = config.tree();
