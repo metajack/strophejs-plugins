b1adcc1661e47d1d02bd691c101052af406cf457
dodo <dodo@blacksec.org>
2011-06-28 02:20:01 +0200

    std lgpl boilerplate

--- register/strophe.register.js
+++ register/strophe.register.js
@@ -1,8 +1,16 @@
 /*
-  This plugin is distributed under the terms of the LGPL licence.
-  Please see the LICENCE file for details.
+This library is free software; you can redistribute it and/or modify it
+ under the terms of the GNU Lesser General Public License as published
+ by the Free Software Foundation; either version 2.1 of the License, or
+ (at your option) any later version.
+ .
+ This library is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+ General Public License for more details.
 
   Copyright (c) dodo <dodo@blacksec.org>, 2011
+
 */
 
 /**
