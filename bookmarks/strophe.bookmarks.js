be085a377d4bddc0de4190bc558a817318f6347f
Miguel Moquillon <miguel.moquillon@silverpeas.com>
2016-12-15 10:22:37 +0100

    The bookmark adding function is also used to update an existing bookmark. So, the code is updated to take into account this use. A fix has been made also for when no nickname is defined in a bookmark (in that case, no need to go up to the parent node with the up() method).

--- bookmarks/strophe.bookmarks.js
+++ bookmarks/strophe.bookmarks.js
@@ -39,7 +39,10 @@ Strophe.addConnectionPlugin('bookmarks', {
 		return true;
 	},
 	/**
-	 * Add bookmark to storage.
+	 * Add bookmark to storage or update it.
+	 *
+	 * The specified room is bookmarked into the remote bookmark storage. If the room is
+	 * already bookmarked, then it is updated with the specified arguments.
 	 *
 	 * @param {string} roomJid - The JabberID of the chat roomJid
 	 * @param {string} [alias] - A friendly name for the bookmark
@@ -63,18 +66,20 @@ Strophe.addConnectionPlugin('bookmarks', {
 			xmlns : Strophe.NS.BOOKMARKS
 		});
 
-		function bookmarkGroupChat() {
-			var conferenceAttr = {
-				jid : roomJid, autojoin : autojoin || false
-			};
+		function bookmarkGroupChat(bookmarkit) {
+			if (bookmarkit) {
+				var conferenceAttr = {
+					jid : roomJid, autojoin : autojoin || false
+				};
 
-			if (alias) {
-				conferenceAttr.name = alias;
-			}
+				if (alias) {
+					conferenceAttr.name = alias;
+				}
 
-			stanza.c('conference', conferenceAttr);
-			if (nick) {
-				stanza.c('nick').t(nick);
+				stanza.c('conference', conferenceAttr);
+				if (nick) {
+					stanza.c('nick').t(nick);
+				}
 			}
 
 			self.connection.sendIQ(stanza, success, error);
@@ -82,26 +87,46 @@ Strophe.addConnectionPlugin('bookmarks', {
 
 		self.get(function(s) {
 			var confs = s.getElementsByTagName('conference');
+			var bookmarked = false;
 			for (var i = 0; i < confs.length; i++) {
 				var conferenceAttr = {
 					jid : confs[i].getAttribute('jid'), autojoin : confs[i].getAttribute('autojoin') || false
 				};
 				var roomName = confs[i].getAttribute('name');
-				if (roomName) {
-					conferenceAttr.name = roomName;
-				}
-				stanza.c('conference', conferenceAttr);
 				var nickname = confs[i].getElementsByTagName('nick');
-				if (nickname.length === 1) {
-					stanza.c('nick').t(nickname[0].innerHTML);
+				
+				if (conferenceAttr.jid === roomJid) {
+					// the room is already bookmarked, then update it
+					bookmarked = true;
+					
+					conferenceAttr.autojoin = autojoin || false;
+					
+					if (alias) {
+						conferenceAttr.name = alias;
+					}
+					stanza.c('conference', conferenceAttr);
+					
+					if (nick) {
+						stanza.c('nick').t(nick).up();
+					}
+				} else {
+					if (roomName) {
+						conferenceAttr.name = roomName;
+					}
+					stanza.c('conference', conferenceAttr);
+				
+					if (nickname.length === 1) {
+						stanza.c('nick').t(nickname[0].innerHTML).up();
+					}
 				}
-				stanza.up().up();
+				
+				stanza.up();
 			}
-
-			bookmarkGroupChat();
+			
+			bookmarkGroupChat(!bookmarked);
 		}, function(s) {
 			if (s.getElementsByTagName('item-not-found').length > 0) {
-				bookmarkGroupChat();
+				bookmarkGroupChat(true);
 			} else {
 				error(s);
 			}
@@ -124,6 +149,9 @@ Strophe.addConnectionPlugin('bookmarks', {
 	},
 	/**
 	 * Delete the bookmark with the given roomJid in the bookmark storage.
+	 *
+	 * The whole remote bookmark storage is just updated by removing the 
+	 * bookmark corresponding to the specified room. 
 	 *
 	 * @param {string} roomJid - The JabberID of the chat roomJid you want to remove
 	 * @param {function} [success] - Callback after success
@@ -160,9 +188,9 @@ Strophe.addConnectionPlugin('bookmarks', {
 				stanza.c('conference', conferenceAttr);
 				var nickname = confs[i].getElementsByTagName('nick');
 				if (nickname.length === 1) {
-					stanza.c('nick').t(nickname[0].innerHTML);
+					stanza.c('nick').t(nickname[0].innerHTML).up();
 				}
-				stanza.up().up();
+				stanza.up();
 			}
 			self.connection.sendIQ(stanza, success, error);
 		}, function(s) {
